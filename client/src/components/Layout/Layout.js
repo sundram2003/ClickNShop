87df941dec37fc11d54aa498ff6eb386bd553b23
Codipk <diptiku2002@gmail.com>
2023-07-03 04:54:54 +0530

    added login and signup page

--- client/src/components/Layout/Layout.js
+++ client/src/components/Layout/Layout.js
@@ -1,36 +1,37 @@
-import React from 'react';
-import Header from '../Header';
-import Footer from '../Footer';
-import {Helmet} from "react-helmet";
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import { Helmet } from "react-helmet";
+// import "react-toastify/dist/ReactToastify.css";
 
+import Toaster from "react-hot-toast";
 
-const Layout = ({children,title,description,keywords,author}) => {
+const Layout = ({ children, title, description, keywords, author }) => {
   return (
     <div>
-        <Helmet>
-            <meta charSet="utf-8" />
-            
-            <meta name="description" content={description} />
-            <meta name="keywords" content={keywords} />
-            <meta name="author" content={author} />
-            <title>{title}</title>
-        </Helmet>
-        <Header/> 
-        <main style={{minHeight: '70vh'}}>
-        {children}
-        </main>
-        <Footer/>
-    </div>
-    
-  )
-}
+      <Helmet>
+        <meta charSet="utf-8" />
 
-export default Layout
+        <meta name="description" content={description} />
+        <meta name="keywords" content={keywords} />
+        <meta name="author" content={author} />
+        <title>{title}</title>
+      </Helmet>
+      <Header />
+      <main style={{ minHeight: "70vh" }}>
+        <Toaster />
+        {children}
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
 
 Layout.defaultProps = {
-    title: "Ecommerce app - shop now",
-    description : "mern stack project",
-    keywords: "mern,react,node,mongodb",
-    author: "Techinfoyt",
-    
-}
+  title: "Ecommerce app - shop now",
+  description: "mern stack project",
+  keywords: "mern,react,node,mongodb",
+  author: "Techinfoyt",
+};
--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,10 +1,15 @@
-import React from 'react'
-import Layout from '../components/Layout/Layout'
+import React from "react";
+import Layout from "../components/Layout/Layout";
+import { useAuth } from "../context/auth";
 
 const HomePage = () => {
+  const [auth, setAuth] = useAuth();
   return (
-    <Layout title={'Best Offers'}>HomePage</Layout>
-  )
-}
+    <Layout title={"Best Offers"}>
+      <h1>Home page</h1>
+      <pre>{JSON.stringify(auth, null, 4)}</pre>
+    </Layout>
+  );
+};
 
-export default HomePage
+export default HomePage;
